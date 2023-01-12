cb578640768a9204476228ce0239a0c8946ff923
John <35427187+JohannLinzen@users.noreply.github.com>
2023-01-12 10:02:14 +0100

    Delete client.js

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import * as contentful from "contentful";
-
-export const client = contentful.createClient({
-  space: '6mdjhxd1fope',
-  accessToken: '0H385gtusCUCK1iEExhrY1kkw6ZfqIIu6qEH3TZnXVU',
-});
